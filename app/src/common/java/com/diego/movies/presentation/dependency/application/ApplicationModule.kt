e5c794d47d25a5cba63a6282043501d873511964
Diego <diegocastma88@gmail.com>
2018-08-02 22:57:49 +0200

    Add http client dependency

--- app/src/common/java/com/diego/movies/presentation/dependency/application/ApplicationModule.kt
+++ app/src/common/java/com/diego/movies/presentation/dependency/application/ApplicationModule.kt
@@ -1,8 +1,13 @@
 package com.diego.movies.presentation.dependency.application
 
 import com.diego.movies.App
+import com.diego.movies.BuildConfig
+import com.diego.movies.R
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -10,4 +15,23 @@ class ApplicationModule(val app: App) {
     @Provides
     @Singleton
     fun provideApp() = app
+    
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() : OkHttpClient {
+        return OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .addInterceptor(getLoggingInterceptor())
+                .build()
+    }
+    
+    private fun getLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+        }
+        
+        return interceptor
+    }
 }
