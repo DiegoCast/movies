3867e0d121387fb4de05bbc27e9f1845e87f5844
Diego <diegocastma88@gmail.com>
2018-08-06 20:54:58 +0200

    Add lifecycle owner and observer

--- app/src/main/java/com/diego/movies/presentation/movies/MoviesActivity.kt
+++ app/src/main/java/com/diego/movies/presentation/movies/MoviesActivity.kt
@@ -1,13 +1,17 @@
 package com.diego.movies.presentation.movies
 
-import android.app.Activity
+import android.arch.lifecycle.LifecycleOwner
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.diego.movies.App
 import com.diego.movies.R
 import com.diego.movies.domain.model.Movie
 import com.diego.movies.presentation.dependency.movies.MoviesModule
+import javax.inject.Inject
 
-class MoviesActivity : Activity (), MoviesView {
+class MoviesActivity : AppCompatActivity (), MoviesView, LifecycleOwner {
+    
+    @Inject lateinit var presenter: MoviesPresenter
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,7 @@ class MoviesActivity : Activity (), MoviesView {
         App.getAppComponent()
                 .plusMoviesComponent(MoviesModule(this))
                 .inject(this)
+        lifecycle.addObserver(presenter)
     }
     
     
--- app/src/main/java/com/diego/movies/presentation/movies/MoviesPresenter.kt
+++ app/src/main/java/com/diego/movies/presentation/movies/MoviesPresenter.kt
@@ -1,10 +1,18 @@
 package com.diego.movies.presentation.movies
 
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
 import com.diego.movies.domain.movies.GetMoviesUseCase
 import javax.inject.Inject
 
 class MoviesPresenter @Inject constructor(private val view: MoviesView,
-                                          private val getMoviesUseCase: GetMoviesUseCase) {
+                                          private val getMoviesUseCase: GetMoviesUseCase) : LifecycleObserver {
+    
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun start() {
+    
+    }
     
     fun next(page: Int) {
     
