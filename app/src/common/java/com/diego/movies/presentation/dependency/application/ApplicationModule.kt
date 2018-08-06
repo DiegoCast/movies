b91ad198f7a39472dbbc2f4591d4a105119884f7
Diego <diegocastma88@gmail.com>
2018-08-06 23:05:54 +0200

    Add schedulers dependencies

--- app/src/common/java/com/diego/movies/presentation/dependency/application/ApplicationModule.kt
+++ app/src/common/java/com/diego/movies/presentation/dependency/application/ApplicationModule.kt
@@ -3,6 +3,8 @@ package com.diego.movies.presentation.dependency.application
 import com.diego.movies.App
 import dagger.Module
 import dagger.Provides
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import java.util.*
 import javax.inject.Named
 import javax.inject.Singleton
@@ -18,4 +20,14 @@ class ApplicationModule(val app: App) {
     @Singleton
     @Named("language")
     fun provideLanguage() = Locale.getDefault().language
+    
+    @Provides
+    @Singleton
+    @Named("main")
+    fun provideMainThread() = AndroidSchedulers.mainThread()
+    
+    @Provides
+    @Singleton
+    @Named("io")
+    fun provideIoThread() = Schedulers.io()
 }
